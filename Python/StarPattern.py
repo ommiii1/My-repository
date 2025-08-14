83d54837fc903d8e55221d53148e1a62b07d7748
ommiii1 <omdhakane0987@gmail.com>
2025-08-14 19:27:12 +0530

    Create StarPattern.py

---
+++ Python/StarPattern.py
@@ -0,0 +1,13 @@
+#star pattern 1
+for i in range(0,6):
+    for j in range(0,i):
+        print("*",end="")
+    print("")
+print("\n")
+
+#star pattern 2
+for i in range(0,6):
+    for j in range(6,i,-1):
+        print("*",end="")
+    print("")
+print("\n")
