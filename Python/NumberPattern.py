48f686e914b0e2a1f8b2ad950468e3c990b4b5ff
ommiii1 <omdhakane0987@gmail.com>
2025-08-14 19:27:52 +0530

    Create NumberPatterns.py

---
+++ Python/NumberPattern.py
@@ -0,0 +1,29 @@
+#number pattern 1
+for i in range(0,6):
+    for j in range(0,i):
+        print(i,end="")
+    print("")
+print("\n")
+
+
+#number pattern 2
+for i in range(0,6):
+    for j in range(0,i):
+        print(j,end="")
+    print("")
+print("\n")
+
+#number pattern 3
+for i in range(0,6):
+    for j in range(6,i,-1):
+        print(i,end="")
+    print("")
+print("\n")
+
+
+#number pattern 4
+for i in range(0,6):
+    for j in range(6,i,-1):
+        print(j,end="")
+    print("")
+print("\n")
