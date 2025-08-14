993c9b932d2c51ccfdc1b4b8da5a7f352f16b69e
ommiii1 <omdhakane0987@gmail.com>
2025-08-14 19:23:10 +0530

    Create sumof2.py

---
+++ Python/sumof2.py
@@ -0,0 +1,16 @@
+
+#sum of 2 numbers
+a=10
+b=20
+c=a+b
+print(c,"\n")
+
+
+#sum of 2 numbers with user input
+print("Enter value of a")
+a = int(input())
+print("Enter value of b")
+b = int(input())
+c=a+b
+print("sum is",c,"\n")
+
