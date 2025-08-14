5d1e88d908f94577c4cdd8117d4090a27acbd077
ommiii1 <omdhakane0987@gmail.com>
2025-08-14 19:24:11 +0530

    Create Percentage.py

---
+++ Python/Percentage.py
@@ -0,0 +1,6 @@
+#Percentage of given marks
+a = int(input("Marks of maths: "))
+b = int(input("Marks of physics: "))
+c = int(input("Marks of chemistry: "))
+d = int(input("Marks of bio: "))
+print("Percentage you scored is: ",((a+b+c+d)*100)/400,"%\n")
